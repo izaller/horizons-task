7ba276bc746f8c34c0615fd1b513d9ff365001ad
Isabel Zaller <isabelzaller@gmail.com>
2020-08-13 09:53:09 -0700

    Update wilson-2ab.py

--- 01_Pilot/scripts/wilson-2ab.py
+++ 01_Pilot/scripts/wilson-2ab.py
@@ -13,16 +13,15 @@ pd.set_option('display.max_columns', 12)
 # load full data
 df = pd.read_csv('../data/data.csv')
 
-
 filt = df.loc[df['Trial'] == 5].filter(items=['Horizon', 'delta', 'Info', 'Choice'])
 
+# split into equal [2,2] and unequal [3,1][1,3] info games
 eq = filt.loc[filt['Info'] == 0].filter(items=['delta', 'Horizon', 'Choice'])
 uneq = filt.loc[filt['Info'] != 0].copy()
 
 # add col to uneq to describe if subject made the more informative choice that round: 1 = made info choice, 0 = did not
-uneq['made_info_choice'] = np.where(((uneq.Choice == 1) & (uneq.Info == 1)) | ((uneq.Choice == 0) & (uneq.Info == -1)),
-                                    1, 0)
-# adjust so that less inf mean = informative mean + delta (rather than right = left + delta)
+uneq['made_info_choice'] = np.where(((uneq.Choice == 1)&(uneq.Info == 1)) | ((uneq.Choice == 0)&(uneq.Info == -1)),1,0)
+# adjust so that (delta = more informative - less informative) rather than (delta = left - right)
 uneq['adjusted_delta'] = np.where((uneq.Info == -1), -uneq.delta, uneq.delta)
 
 h1_eq = eq.loc[eq['Horizon'] == 5].filter(items=['delta', 'Choice']).groupby('delta').mean('Choice')
@@ -32,7 +31,6 @@ h1_uneq = uneq.loc[uneq['Horizon'] == 5].filter(items=['adjusted_delta', 'made_i
     .mean('made_info_choice')
 h6_uneq = uneq.loc[uneq['Horizon'] == 10].filter(items=['adjusted_delta', 'made_info_choice'])\
     .groupby('adjusted_delta').mean('made_info_choice')
-
 
 # plot the data
 fig, (unequal, equal) = plt.subplots(1, 2, figsize=(16, 4))
