616ef9be66b8ff2c74e81cf850084774e5960592
Isabel Zaller <izaller@princeton.edu>
2020-10-02 09:59:25 -0400

    demo stan code

---
+++ pilot-v0.2/scripts/demostan.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from stantools.io import load_model
+import seaborn as sns
+
+sns.set_style('white')
+
+## Define parameters.
+b0 = -1
+b1 = 2
+
+
+## Define logistic function.
+def inv_logit(x):
+    return 1. / (1 + np.exp(-x))
+
+
+## Define observed variables.
+x = np.linspace(-5, 5, 500)
+
+## Define probability of choice.
+theta = inv_logit(b0 + b1 * x)
+
+## Simulate choice.
+y = np.random.binomial(1, theta)
+
+plt.plot(x, theta, '-')
+
+## Compile model.
+StanModel = load_model('/Users/isabelzaller/Desktop/GitHub/horizons-task/pilot-v0.2/scripts/logistic.stan')
+
+## Prepare data for Stan.
+dd = dict(T=x.size, x=x, y=y)
+
+## Fit model.
+StanFit = StanModel.optimizing(data=dd, seed=0)
+print(StanFit)
