5f6756c781b637d5661e9f9430c7c206bff395c6
Isabel Zaller <izaller@princeton.edu>
2021-01-06 16:44:53 -0800

    Update format-metadata.py

--- pilot-v0.2/scripts/metadata-analysis/format-metadata.py
+++ pilot-v0.2/scripts/metadata-analysis/format-metadata.py
@@ -1,6 +1,6 @@
 import pandas as pd
-import statistics
-
+import matplotlib.pyplot as plt
+from scipy import stats
 
 meta = pd.read_csv('/Users/isabelzaller/Desktop/GitHub/horizons-task/pilot-v0.2/data/metadata.csv')
 
@@ -80,6 +80,37 @@ pswq = pd.Series(get_pswq())
 ius = pd.Series(get_ius12())
 ncs = pd.Series(get_ncs(fullrow=True))
 
+
+fig, (pswq_ius, ius_ncs, ncs_pswq) = plt.subplots(nrows=1, ncols=3, sharey='row', figsize=(16, 4))
+
+pswq_ius.plot(pswq, ius, 'o')   ## 1
+ius_ncs.plot(pswq, ncs, 'o')    ## 2
+ncs_pswq.plot(ncs, pswq, 'o')   ## 3
+
+print(stats.spearmanr(pswq, ius))
+print(stats.spearmanr(ius, ncs))
+print(stats.spearmanr(ncs, pswq))
+
+# rho_1, p1 = stats.spearmanr(pswq, ius)
+# rho_2, p2 = stats.spearmanr(ius, ncs)
+# rho_3, p3 = stats.spearmanr(ncs, pswq)
+
+fig.suptitle('Anxiety questionnaire correlations')
+pswq_ius.set(title='PSWQ vs. IUS-12')
+ius_ncs.set(title='IUS-12 vs. NCS')
+ncs_pswq.set(title='NCS vs. PSWQ')
+fig.tight_layout(pad=2.5)
+
+# corr = '''Spearman correlations
+    #             H1: rho = %f, p = %f
+    #             H6: rho = %f, p = %f''' % (rho_1, p1, rho_6, p6)
+    # alpha1.set(title='Horizon 1', xlabel='%s score' % test, ylabel='Information bonus')
+    # alpha6.set(title='Horizon 6', xlabel='%s score' % test)
+    # fig1.suptitle('%s score vs. information bonus' % test, fontsize=16)
+    # fig1.text(.5, 0.01, corr, ha='center', fontsize=8)
+    # fig1.tight_layout(pad=2.5)
+    # fig1.savefig(figname + '-alpha.png')
+
 # pswq_mean = avg(pswq)
 # pswq_sd = statistics.stdev(pswq)
 #
@@ -92,9 +123,9 @@ ncs = pd.Series(get_ncs(fullrow=True))
 # print('pswq mean: %d' % pswq_mean)
 # print('ius-12 mean: %d' % ius_mean)
 # print('ncs mean: %d' % ncs_mean)
-print('pswq')
-print(pswq.quantile([0, .25, .5, .75, 1]))
-print('ius')
-print(ius.quantile([0, .25, .5, .75, 1]))
-print('ncs')
-print(ncs.quantile([0, .25, .5, .75, 1]))
+# print('pswq')
+# print(pswq.quantile([0, .25, .5, .75, 1]))
+# print('ius')
+# print(ius.quantile([0, .25, .5, .75, 1]))
+# print('ncs')
+# print(ncs.quantile([0, .25, .5, .75, 1]))
